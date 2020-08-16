066670e1c2686d4929dd52e3066122b52a172bc8
Jishnu M A <jishnu.m@travelyaari.com>
2020-08-16 23:19:25 +0530

    Added Viewstate Added LifeCycle Listners

--- app/src/main/java/com/example/coronastats/ui/indiacovidstats/CovidIndiaStatsFragment.kt
+++ app/src/main/java/com/example/coronastats/ui/indiacovidstats/CovidIndiaStatsFragment.kt
@@ -1,18 +1,19 @@
 package com.example.coronastats.ui.indiacovidstats
 
-import android.app.Application
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coronastats.CovidMainScreenActivity
-import com.example.coronastats.R
 import com.example.coronastats.databinding.FragmentCovidIndiaStatsBinding
 import com.example.coronastats.ui.BaseFragment
+import com.example.coronastats.ui.viewstate.CountryViewState
 
 class CovidIndiaStatsFragment : BaseFragment() {
 
@@ -31,28 +32,48 @@ class CovidIndiaStatsFragment : BaseFragment() {
         binding = FragmentCovidIndiaStatsBinding.inflate(inflater, container, false)
 
         navController = (activity as CovidMainScreenActivity).getNavController()
-
-
         indiaStatsViewModel =
-            IndiaStatsViewModel(application = Application())
-
-
+            ViewModelProvider(requireActivity()).get(IndiaStatsViewModel::class.java)
 
         indiaStatsViewModel.callCovidStatsAPI()
-        indiaStatsViewModel.getIndiaCovidStats().observe(activity as CovidMainScreenActivity,
-            Observer {
-                if (it.isSuccessful) {
-                    Log.v("Success", "Called successfully")
-                    adapter = IndiaCovidStatusAdapter()
-                    binding!!.rcvIndiaData.layoutManager =
-                        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                    adapter.setData(it.body())
-                    binding!!.rcvIndiaData.adapter = adapter
-                }
-            })
-
+        observerChangesFromVM();
 
         return binding.root
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun observerChangesFromVM() {
+        indiaStatsViewModel.getIndiaCovidStats().observe(activity as CovidMainScreenActivity,
+            Observer {
+                when (it) {
+                    CountryViewState.PROGRESS -> showProgress()
+                    CountryViewState.FAILED -> showError(it.error)
+                    CountryViewState.SUCCESS -> {
+                        showSuccess()
+                        adapter = IndiaCovidStatusAdapter()
+                        binding!!.rcvIndiaData.layoutManager =
+                            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                        adapter.setData(it.data?.body())
+                        binding!!.rcvIndiaData.adapter = adapter
+                    }
+                }
+            })
+    }
+
+    fun showProgress() {
+        Thread.sleep(1000)
+        binding.progressbar.visibility = View.VISIBLE
+        binding.error.visibility = View.GONE
+    }
+
+    fun showError(error: String?) {
+        binding.progressbar.visibility = View.GONE
+        binding.error.text = error ?: "Failed to fetch fata"
+        binding.rcvIndiaData.visibility = View.GONE
+    }
+
+    fun showSuccess() {
+        binding.progressbar.visibility = View.GONE
+        binding.error.visibility = View.GONE
+    }
 }
