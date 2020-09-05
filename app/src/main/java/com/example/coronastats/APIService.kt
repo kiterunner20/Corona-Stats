bebae1911136c88aed984710c39a83c8e85f18bf
Jishnu <Jishnu88adi@gmail.com>
2020-09-06 01:55:24 +0530

    Refactoring -removed some dummy classes

--- app/src/main/java/com/example/coronastats/APIService.kt
+++ app/src/main/java/com/example/coronastats/APIService.kt
@@ -1,14 +1,10 @@
 package com.example.coronastats
 
-import com.example.coronastats.data.CountryWiseStatsItem
 import com.example.coronastats.data.latestcasesindia.CovidStatsInfo
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface APIService {
-
-    @GET("countries")
-    suspend fun getCoronaStatsOnCountries(): Response<List<CountryWiseStatsItem>>
 
     @GET("stats/latest")
     suspend fun getIndiaStats(): Response<CovidStatsInfo>
